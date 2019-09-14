98f70b4d8fa784af8df1bc3e8e90be11b2be1829
MarcosLudgerio <raimundo.marcos@dce.ufpb.br>
2019-09-14 19:02:33 -0300

    Adiocionado arquivo para troca de likes entre os devs

--- curso/src/controllers/DevController.js
+++ curso/src/controllers/DevController.js
@@ -1,11 +1,28 @@
 const axios = require('axios');
 
+const Dev = require('../models/Dev');
+
+
 module.exports = {
     async store(req, res) {
-        const {nome} = req.body;
-        
+        const { nome } = req.body;
+
+        const userExist = await Dev.findOne({ user:nome })
+        if(userExist){
+            return res.json(userExist);
+        }
+
         const response = await axios.get(`https://api.github.com/users/${nome}`)
 
-        return res.json(response.data);
+        const { name, bio, avatar_url } = response.data;
+
+        const dev = await Dev.create({
+            nome: name,
+            user: nome,
+            bio,
+            avatar: avatar_url
+        });
+
+        return res.json(dev);
     }
 };
